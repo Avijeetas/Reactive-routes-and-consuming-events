2195ffeddd4110c1ad9bb59e62cb1255ee0ea6b9
Avijeet Shil <avijeet.vivasoft@bkash.com>
2022-12-01 16:02:42 +0600

    adding route

--- src/main/java/com/example/reactiveroutes/config/RouterConfig.java
+++ src/main/java/com/example/reactiveroutes/config/RouterConfig.java
@@ -1,8 +1,32 @@
 package com.example.reactiveroutes.config;
 
+import com.example.reactiveroutes.model.Product;
+import com.example.reactiveroutes.service.ProductService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
 public class RouterConfig {
+    private final ProductService productService;
 
+    public RouterConfig(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> router(){
+        return route()
+                .GET("/products",
+                        request -> ok()
+                                .contentType(MediaType.TEXT_EVENT_STREAM) // very important as it is a reactive programming
+                                .body(productService.getAll(), Product.class))
+                .build();
+
+    }
 }
