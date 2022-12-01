f0809b7f33e1f2bf04c933a49c374169741d7e3b
Avijeet Shil <avijeet.vivasoft@bkash.com>
2022-12-01 13:01:50 +0600

    bootstrapping the project

---
+++ src/test/java/com/example/reactiveroutes/ReactiveRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveRoutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
